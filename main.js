5c564403046c2e8bf60be6d5edda4d9ea0da2453
NaviDVC <ivan999az.vf@gmail.com>
2023-05-31 16:28:59 +0300

    added html and js

---
+++ main.js
@@ -0,0 +1,4 @@
+let hours = prompt("Hello, type the number of hours:");
+hours = parseInt(hours);
+let seconds = hours * 60 * 60;
+alert("It's" + " " + seconds + " " + "seconds" + " "  + "in" + " " + hours + " " + "hours.")
